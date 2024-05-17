1cbf0723bae17cf3e99d6e4c85d6b1c55fb76bf4
Vinayaks439 <vinayaks439@gmail.com>
2024-05-17 16:29:53 +0530

    added two more folders

---
+++ intergeroverflow/overflow.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2147483647;  // Maximum value for a signed int
+    int b = 1;
+    int result = a + b;
+   
+    printf("Result: %d\n", result);
+   
+    return 0;
+}
